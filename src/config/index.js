d40d1ebd52e7fa9310ca3275d172c7187f8dd113
Dmitriy Kurapin <dmytrokurapin@gmail.com>
2021-04-12 17:19:56 +0300

    separate enviromnents on dev and prod; added usage to scripts accordingly

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 module.exports = {
   endpoint: process.env.API_URL,
   masterKey: process.env.API_KEY,
